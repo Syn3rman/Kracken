b0183f1311cde86f47d8bee1ab4d2f54ec32de5d
Aditya <aditya10699@gmail.com>
2019-12-31 23:22:17 +0530

    improve speed, accept user input

--- main.go
+++ main.go
@@ -5,44 +5,66 @@ import (
 	"os"
 	"crypto/md5"
 	"encoding/hex"
-	// "time"
+	"sync"
+	"time"
+	"runtime"
+	"bufio"
+	"log"
 	// "reflect"
 )
-
-// MAXCOUNT -> Max number of goroutines
-const MAXCOUNT = 1000000
-var sem = make(chan int, MAXCOUNT)
 
 func compareHash(str string, inputHash string){
 	hash := md5.Sum([]byte(str))
 	calcHash := string(hex.EncodeToString(hash[:])[:])
-	fmt.Println("Checking md5(", str, ")against ", inputHash)   
+	fmt.Println("Checking md5(",str,")against ", inputHash)   
 	if calcHash == inputHash{
-		fmt.Println("--------------------MATCH FOUND--------------------------------", str)
+		fmt.Println("Match found: ", str)
 		os.Exit(0);
-		// return true;
 	}
-	<-sem
 }
 
-func allStr(s []string, prefix string, n,k int){
+func allStr(s []string, prefix, inputHash string, n,k int, wg *sync.WaitGroup){
+	defer wg.Done();
 	if k==0{
-		sem <- 1
-		go compareHash(prefix, "26e9262dceb7a2c4e1dfc417b5234370")
+		compareHash(prefix, inputHash)
 		return;
 	}
 	for i:=0;i<n;i++{
 		newPrefix := prefix + s[i]
-		allStr(s, newPrefix, n, k-1)
+		wg.Add(1)
+		allStr(s, newPrefix, inputHash, n, k-1, wg)
 	}
 }
 
 func main() {
-	// chars:=[]string{"a","b","c","d","e","1","2","3","4"}
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	chars:=[]string{
-		/*"@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-		*/"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+		"@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
 		"1","2","3","4","5","6","7","8","9","0" }
-	k,n:=7,len(chars)
-	allStr(chars,"" , n, k)
+	
+	var k int
+	n:=len(chars)
+	var wg sync.WaitGroup
+	
+	//User input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Input hash: ")
+	inputHash, _ := reader.ReadString('\n')
+	fmt.Print("Length of password: ")
+	_, err := fmt.Scanf("%d", &k)
+	if err!=nil {
+		log.Print("Scan for k failed due to", err)
+		return
+	}
+
+	start := time.Now()
+	for ind := range chars{
+		wg.Add(1)
+		go allStr(append(chars[ind:],chars[:ind]...),"", inputHash, n, k, &wg)
+	}
+	wg.Wait()
+	
+	elapsed := time.Since(start)
+	fmt.Printf("Cracked hash in %s", elapsed)
 }
